1ef170d743fe2d37d706fa1ff23379da3bcf2bcb
josemmc06 <106336939+josemmc06@users.noreply.github.com>
2023-05-29 09:00:33 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Molina")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Carrillo")
+})
+basic.showString("Jose Manuel")
